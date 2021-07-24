7bb3ad9f94b11c1021f7e48f7ef83f572f423821
Gonzalez32 <3232rose@gmail.com>
2021-07-23 21:18:30 -0500

    modified views.py

--- findweather/views.py
+++ findweather/views.py
@@ -1,15 +1,42 @@
 import requests
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import City
 from .forms import CityForm
 
 # Create your views here.
 def home(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=YOUR_API_KEY'
+    # url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=YOUR_API_KEY'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=f7559e8dce1a0f28232101edcca98a0a'
+
+
+    err_msg = ''
+    message = ''
+    message_class = ''
 
     if request.method == 'POST':
         form = CityForm(request.POST)
-        form.save()
+        # form.save()
+
+        if form.is_valid():
+            new_city = form.cleaned_data['name']
+            existing_city_count = City.objects.filter(name=new_city).count()
+
+            if existing_city_count == 0:
+                get = requests.get(url.format(new_city)).json()
+
+                if get['cod'] == 200:
+                    form.save()
+                else:
+                    err_msg = 'Sorry city does not exist in the world!'
+            else:
+                err_msg = 'City already exists in the database!'
+    
+    if err_msg:
+        message = err_msg
+        message_class = 'is-danger'
+    else:
+        message = 'City added successfully!'
+        message_class = 'is-success'
 
     form = CityForm()
 
@@ -30,8 +57,19 @@ def home(request):
 
         weather_data.append(city_weather)
 
-    context = {'weather_data' : weather_data, 'form' : form}
+    context = {
+        'weather_data' : weather_data, 
+        'form' : form,
+        'message' : message,
+        'message_class' : message_class
+    }
+
     return render(request, 'home.html', context)
+
+def delete_city(request, city_name):
+    City.objects.get(name=city_name).delete()
+
+    return redirect('home')
 
 
 # def about(request):
